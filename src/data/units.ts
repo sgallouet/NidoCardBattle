a305103f6cf1e8929d61a15b4b0db1e8451a8fb7
sgallouet <41896688+sgallouet@users.noreply.github.com>
2026-08-22 10:03:12 +0900

    Add complete Human and Undead unit roster

--- src/data/units.ts
+++ src/data/units.ts
@@ -2,6 +2,8 @@ import type { UnitDefinition } from './types';
 
 export type UnitDefinitionId =
   | 'commander'
+  | 'humanCommander'
+  | 'undeadCommander'
   | 'skeletonGuard'
   | 'boneArcher'
   | 'vampire'
@@ -9,31 +11,44 @@ export type UnitDefinitionId =
   | 'banshee'
   | 'wraith'
   | 'ghoul'
-  | 'graveKnight';
+  | 'graveKnight'
+  | 'bannerCaptain'
+  | 'windAdept'
+  | 'undeadBoneArcher'
+  | 'undeadVampire'
+  | 'undeadWraith';
 
 export const UNIT_DEFINITIONS: Record<UnitDefinitionId, UnitDefinition> = {
   commander: {
     id: 'commander', name: 'Commander', faction: 'shared', cost: 0, maxHp: 10, attack: 3, move: 2, range: 1,
-    traits: ['Blocking', 'Retaliates'], mark: 'C',
+    traits: ['Blocking'], mark: 'C',
+  },
+  humanCommander: {
+    id: 'commander', name: 'Human Commander', faction: 'human', cost: 0, maxHp: 10, attack: 3, move: 2, range: 1,
+    traits: ['Blocking', 'Retaliates'], ability: 'Rally', mark: 'C',
+  },
+  undeadCommander: {
+    id: 'commander', name: 'Undead Commander', faction: 'undead', cost: 0, maxHp: 10, attack: 3, move: 2, range: 1,
+    traits: ['Blocking', 'DarkReflection'], ability: 'SoulLink', mark: 'C',
   },
   skeletonGuard: {
     id: 'skeletonGuard', name: 'Skeletal Infantry', faction: 'undead', cost: 1, maxHp: 2, attack: 2, move: 2, range: 1,
-    traits: ['Blocking'], mark: 'S',
+    traits: ['Blocking', 'Assist'], mark: 'S',
   },
   boneArcher: {
-    id: 'boneArcher', name: 'Longbow Ranger', faction: 'human', cost: 3, maxHp: 2, attack: 2, move: 2, range: 3,
-    traits: ['Ranged'], mark: 'L',
+    id: 'boneArcher', name: 'Longbow Ranger', faction: 'human', cost: 3, maxHp: 1, attack: 1, move: 2, range: 3,
+    traits: ['Ranged', 'Assist'], mark: 'L',
   },
   vampire: {
     id: 'vampire', name: 'Silverwing Cavalry', faction: 'human', cost: 6, maxHp: 5, attack: 4, move: 4, range: 1,
-    traits: ['Flying', 'Charge'], mark: 'S',
+    traits: ['Flying', 'AgileAssault'], mark: 'S',
   },
   necromancer: {
-    id: 'necromancer', name: 'Necromancer', faction: 'undead', cost: 5, maxHp: 4, attack: 1, move: 2, range: 2,
-    traits: ['Invoker'], mark: 'N',
+    id: 'necromancer', name: 'Necromancer', faction: 'undead', cost: 5, maxHp: 4, attack: 1, move: 2, range: 3,
+    traits: ['Invoker', 'Ranged', 'Necromancy'], ability: 'Curse', mark: 'N',
   },
   banshee: {
-    id: 'banshee', name: 'Banshee Displacer', faction: 'undead', cost: 4, maxHp: 3, attack: 2, move: 3, range: 1,
+    id: 'banshee', name: 'Banshee', faction: 'undead', cost: 4, maxHp: 3, attack: 2, move: 3, range: 1,
     traits: [], ability: 'Displace', mark: 'B',
   },
   wraith: {
@@ -42,10 +57,30 @@ export const UNIT_DEFINITIONS: Record<UnitDefinitionId, UnitDefinition> = {
   },
   ghoul: {
     id: 'ghoul', name: 'Royal Guard', faction: 'human', cost: 2, maxHp: 3, attack: 2, move: 2, range: 1,
-    traits: ['Blocking', 'Retaliates'], mark: 'R',
+    traits: ['Blocking', 'Retaliates', 'Assist'], mark: 'R',
   },
   graveKnight: {
-    id: 'graveKnight', name: 'Grave Knight', faction: 'undead', cost: 5, maxHp: 4, attack: 4, move: 2, range: 1,
-    traits: ['Blocking', 'Retaliates'], mark: 'K',
+    id: 'graveKnight', name: 'Grave Knight', faction: 'undead', cost: 5, maxHp: 5, attack: 3, move: 2, range: 1,
+    traits: ['Blocking', 'Retaliates'], ability: 'Cleave', mark: 'K',
+  },
+  bannerCaptain: {
+    id: 'bannerCaptain', name: 'Banner Captain', faction: 'human', cost: 4, maxHp: 4, attack: 2, move: 2, range: 1,
+    traits: ['Invoker'], mark: 'B',
+  },
+  windAdept: {
+    id: 'windAdept', name: 'Wind Adept', faction: 'human', cost: 3, maxHp: 2, attack: 1, move: 3, range: 2,
+    traits: [], ability: 'Displace', mark: 'W',
+  },
+  undeadBoneArcher: {
+    id: 'undeadBoneArcher', name: 'Bone Archer', faction: 'undead', cost: 3, maxHp: 1, attack: 1, move: 2, range: 3,
+    traits: ['Ranged', 'Assist'], mark: 'A',
+  },
+  undeadVampire: {
+    id: 'undeadVampire', name: 'Vampire', faction: 'undead', cost: 5, maxHp: 4, attack: 3, move: 3, range: 1,
+    traits: ['Flying'], ability: 'BloodDrain', mark: 'V',
+  },
+  undeadWraith: {
+    id: 'undeadWraith', name: 'Wraith', faction: 'undead', cost: 4, maxHp: 3, attack: 2, move: 4, range: 1,
+    traits: ['Phase'], mark: 'W',
   },
 };

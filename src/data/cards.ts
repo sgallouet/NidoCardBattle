377b7893ad98a80b746b4e5df1d9ec8288748a20
sgallouet <41896688+sgallouet@users.noreply.github.com>
2026-08-22 09:08:21 +0900

    TASK-001 split decks by faction

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,15 +1,20 @@
-import type { CardDefinition } from './types';
+import type { CardDefinition, Faction } from './types';
 import { UNIT_DEFINITIONS, type UnitDefinitionId } from './units';
 
 type PrototypeUnitId = Exclude<UnitDefinitionId, 'commander'>;
 
-const unitCard = <T extends PrototypeUnitId>(unitId: T) => ({
-  id: unitId,
-  name: UNIT_DEFINITIONS[unitId].name,
-  type: 'unit' as const,
-  cost: UNIT_DEFINITIONS[unitId].cost,
-  unitId,
-});
+const unitCard = <T extends PrototypeUnitId>(unitId: T) => {
+  const unit = UNIT_DEFINITIONS[unitId];
+  if (unit.faction === 'shared') throw new Error(`${unit.name} cannot be used as a faction card.`);
+  return {
+    id: unitId,
+    name: unit.name,
+    faction: unit.faction,
+    type: 'unit' as const,
+    cost: unit.cost,
+    unitId,
+  };
+};
 
 export const CARD_DEFINITIONS = {
   skeletonGuard: unitCard('skeletonGuard'),
@@ -24,13 +29,17 @@ export const CARD_DEFINITIONS = {
 
 export type CardDefinitionId = keyof typeof CARD_DEFINITIONS;
 
-export const PROTOTYPE_DECK: CardDefinitionId[] = [
-  'skeletonGuard', 'skeletonGuard',
-  'boneArcher', 'boneArcher',
-  'vampire', 'vampire',
-  'necromancer', 'necromancer',
-  'banshee', 'banshee',
-  'wraith', 'wraith',
-  'ghoul', 'ghoul',
-  'graveKnight', 'graveKnight',
-];
+export const FACTION_DECKS: Record<Faction, CardDefinitionId[]> = {
+  human: [
+    'boneArcher', 'boneArcher',
+    'vampire', 'vampire',
+    'wraith', 'wraith',
+    'ghoul', 'ghoul',
+  ],
+  undead: [
+    'skeletonGuard', 'skeletonGuard',
+    'necromancer', 'necromancer',
+    'banshee', 'banshee',
+    'graveKnight', 'graveKnight',
+  ],
+};

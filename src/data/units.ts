8aa29c2b5170fc7d3c3e34ed1b4a719cec4a71b9
sgallouet <41896688+sgallouet@users.noreply.github.com>
2026-08-22 09:08:10 +0900

    TASK-001 assign units to factions

--- src/data/units.ts
+++ src/data/units.ts
@@ -13,39 +13,39 @@ export type UnitDefinitionId =
 
 export const UNIT_DEFINITIONS: Record<UnitDefinitionId, UnitDefinition> = {
   commander: {
-    id: 'commander', name: 'Commander', cost: 0, maxHp: 10, attack: 3, move: 2, range: 1,
+    id: 'commander', name: 'Commander', faction: 'shared', cost: 0, maxHp: 10, attack: 3, move: 2, range: 1,
     traits: ['Blocking', 'Retaliates'], mark: 'C',
   },
   skeletonGuard: {
-    id: 'skeletonGuard', name: 'Skeletal Infantry', cost: 1, maxHp: 2, attack: 2, move: 2, range: 1,
+    id: 'skeletonGuard', name: 'Skeletal Infantry', faction: 'undead', cost: 1, maxHp: 2, attack: 2, move: 2, range: 1,
     traits: ['Blocking'], mark: 'S',
   },
   boneArcher: {
-    id: 'boneArcher', name: 'Longbow Ranger', cost: 3, maxHp: 2, attack: 2, move: 2, range: 3,
+    id: 'boneArcher', name: 'Longbow Ranger', faction: 'human', cost: 3, maxHp: 2, attack: 2, move: 2, range: 3,
     traits: ['Ranged'], mark: 'L',
   },
   vampire: {
-    id: 'vampire', name: 'Silverwing Cavalry', cost: 6, maxHp: 5, attack: 4, move: 4, range: 1,
+    id: 'vampire', name: 'Silverwing Cavalry', faction: 'human', cost: 6, maxHp: 5, attack: 4, move: 4, range: 1,
     traits: ['Flying', 'Charge'], mark: 'S',
   },
   necromancer: {
-    id: 'necromancer', name: 'Necromancer', cost: 5, maxHp: 4, attack: 1, move: 2, range: 2,
+    id: 'necromancer', name: 'Necromancer', faction: 'undead', cost: 5, maxHp: 4, attack: 1, move: 2, range: 2,
     traits: ['Invoker'], mark: 'N',
   },
   banshee: {
-    id: 'banshee', name: 'Banshee Displacer', cost: 4, maxHp: 3, attack: 2, move: 3, range: 1,
+    id: 'banshee', name: 'Banshee Displacer', faction: 'undead', cost: 4, maxHp: 3, attack: 2, move: 3, range: 1,
     traits: [], ability: 'Displace', mark: 'B',
   },
   wraith: {
-    id: 'wraith', name: 'Light Mage', cost: 4, maxHp: 3, attack: 2, move: 2, range: 2,
+    id: 'wraith', name: 'Light Mage', faction: 'human', cost: 4, maxHp: 3, attack: 2, move: 2, range: 2,
     traits: [], ability: 'Restore', mark: 'M',
   },
   ghoul: {
-    id: 'ghoul', name: 'Royal Guard', cost: 2, maxHp: 3, attack: 2, move: 2, range: 1,
+    id: 'ghoul', name: 'Royal Guard', faction: 'human', cost: 2, maxHp: 3, attack: 2, move: 2, range: 1,
     traits: ['Blocking', 'Retaliates'], mark: 'R',
   },
   graveKnight: {
-    id: 'graveKnight', name: 'Grave Knight', cost: 5, maxHp: 4, attack: 4, move: 2, range: 1,
+    id: 'graveKnight', name: 'Grave Knight', faction: 'undead', cost: 5, maxHp: 4, attack: 4, move: 2, range: 1,
     traits: ['Blocking', 'Retaliates'], mark: 'K',
   },
 };

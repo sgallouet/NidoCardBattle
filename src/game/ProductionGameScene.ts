32085308227c3e42e2c2ed897d16e1c8f644b8ad
sgallouet <41896688+sgallouet@users.noreply.github.com>
2026-08-26 20:39:26 +0900

    Use cel-shaded river presentation

--- src/game/ProductionGameScene.ts
+++ src/game/ProductionGameScene.ts
@@ -1,16 +1,47 @@
+import Phaser from 'phaser';
+import type { Coord } from '../data/types';
+import {
+  CelShadedRiverSurface,
+  type CelShadedRiverSceneInternals,
+} from './CelShadedRiverSurface';
 import { PlayerCameraChoreographyGameScene } from './PlayerCameraChoreographyGameScene';
 import { SettingsMenu } from './SettingsMenu';
 
+interface ProductionSceneInternals extends CelShadedRiverSceneInternals {
+  addRiverSurface: () => void;
+  clearRiverSurface: () => void;
+  boardLayer?: Phaser.GameObjects.Container;
+  center: (coord: Coord) => Phaser.Math.Vector2;
+  hexPoints: (center: Phaser.Math.Vector2, inset?: number) => Phaser.Geom.Point[];
+}
+
 export class ProductionGameScene extends PlayerCameraChoreographyGameScene {
   private settingsMenu?: SettingsMenu;
+  private celRiver?: CelShadedRiverSurface;
 
   create(): void {
+    const game = this as unknown as ProductionSceneInternals;
+
+    // Replace only the visual river surface before GameScene performs its first render.
+    // Terrain topology, bridge rules and movement remain owned by the existing engine.
+    game.clearRiverSurface = () => {
+      this.celRiver?.destroy();
+      this.celRiver = undefined;
+    };
+    game.addRiverSurface = () => {
+      this.celRiver?.destroy();
+      this.celRiver = new CelShadedRiverSurface(this, game);
+      this.celRiver.render();
+    };
+
     super.create();
 
     this.settingsMenu = new SettingsMenu();
     this.settingsMenu.install();
 
     this.events.once('shutdown', () => {
+      this.celRiver?.destroy();
+      this.celRiver = undefined;
       this.settingsMenu?.destroy();
       this.settingsMenu = undefined;
     });
